394c6abdeacf94c3d446f4f6c1ad0aaf3f1a7b7c
Antonio Zanotto <antonio.zanotto@gmail.com>
2018-05-02 11:27:59 -0300

    Projeto crioado

---
+++ src/main/java/com/aez/cursomc/CurosomcApplication.java
@@ -0,0 +1,12 @@
+package com.aez.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurosomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurosomcApplication.class, args);
+	}
+}
